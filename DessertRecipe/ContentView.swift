0c99663061a0bddc314d098fc7a210e5cde49350
sunnyleeyun <sunnylee945@hotmail.com>
2023-11-08 16:30:29 -0500

    Initial Commit

---
+++ DessertRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertRecipe
+//
+//  Created by 李昀 on 2023/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DessertRecipe/DessertRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipeApp.swift
+//  DessertRecipe
+//
+//  Created by 李昀 on 2023/11/8.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
