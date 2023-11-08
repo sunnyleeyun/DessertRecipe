526ea99fa95c161a897cba4ba219e183eecdfb86
sunnyleeyun <sunnylee945@hotmail.com>
2023-11-08 18:19:24 -0500

    feat: add Meal, Recipe model and NetworkManger file

---
+++ DessertRecipe/Managers/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  DessertRecipe
+//
+//  Created by 李昀 on 2023/11/8.
+//
+
+import Foundation
+
+final class NetworkManager {
+  private let baseUrl = "https://themealdb.com/api/json/v1/1/"
+  func fetchDessertRecipe(handler: @escaping ([Meal]) -> Void) {
+    
+  }
+}
---
+++ DessertRecipe/Models/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  DessertRecipe
+//
+//  Created by 李昀 on 2023/11/8.
+//
+
+import Foundation
+
+struct Meal: Codable {
+  let strMeal: String
+  let strMealThumb: String
+  let idMeal: String
+}
