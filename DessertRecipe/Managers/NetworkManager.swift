51bc9cd3cc8612d4042c396c5251445ad0397fba
sunnyleeyun <sunnylee945@hotmail.com>
2023-11-08 18:57:46 -0500

    feat: add generic network manager

--- DessertRecipe/Managers/NetworkManager.swift
+++ DessertRecipe/Managers/NetworkManager.swift
@@ -7,12 +7,55 @@
 
 import Foundation
 
-final class NetworkManager {
-  private let baseUrl = "https://themealdb.com/api/json/v1/1/"
+// @TODO: Add doc, and design rationale
+class NetworkManager {
   
+  enum ManagerErrors: Error {
+    case invalidResponse
+    case invalidStatusCode(Int)
+  }
   
+  enum HttpMethod: String {
+    case get
+    
+    var method: String { rawValue.uppercased() }
+  }
   
-  func fetchDessertRecipe(handler: @escaping ([Meal]) -> Void) {
-    let url = URL(string: baseUrl + "/filter.php?c=Dessert")
+  func request<T: Decodable>(fromURL url: URL, httpMethod: HttpMethod = .get, completion: @escaping (Result<T, Error>) -> Void) {
+    
+    // URLSession returns on the queue it creates for the request, thus ensure same queue
+    let completionOnMain: (Result<T, Error>) -> Void = { result in
+      DispatchQueue.main.async {
+        completion(result)
+      }
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = httpMethod.method
+    
+    let urlSession = URLSession.shared.dataTask(with: request) { data, response, error in
+      
+      if let error = error {
+        completionOnMain(.failure(error))
+        return
+      }
+      
+      
+      guard let urlResponse = response as? HTTPURLResponse else { return completionOnMain(.failure(ManagerErrors.invalidResponse)) }
+      if !(200..<300).contains(urlResponse.statusCode) {
+        return completionOnMain(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
+      }
+      
+      guard let data = data else { return }
+      do {
+        let users = try JSONDecoder().decode(T.self, from: data)
+        completionOnMain(.success(users))
+      } catch {
+        debugPrint("Unable to decode the data to the requested type. Reason: \(error.localizedDescription)")
+        completionOnMain(.failure(error))
+      }
+    }
+    
+    urlSession.resume()
   }
 }
