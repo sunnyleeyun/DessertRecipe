419e489fc82a12cf8e4beea246084fb6fe34408c
sunnyleeyun <sunnylee945@hotmail.com>
2023-11-08 19:09:54 -0500

    refactor: model to two models

---
+++ DessertRecipe/Features/Home/Model/Meals.swift
@@ -0,0 +1,19 @@
+//
+//  Meal.swift
+//  DessertRecipe
+//
+//  Created by 李昀 on 2023/11/8.
+//
+
+import Foundation
+
+struct Meals: Codable {
+  let meals: [Meal]
+}
+
+struct Meal: Codable {
+  let strMeal: String
+  let strMealThumb: String
+  let idMeal: String
+}
+
